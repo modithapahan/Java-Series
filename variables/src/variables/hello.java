0aae23266a47b1d6b6cfbadd4bfe2d074de1145f
modithapahan <modithapahan0@gmail.com>
2022-08-16 22:38:14 +0530

    Variables & Data Types

---
+++ variables/src/variables/hello.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class hello {
+	public static void main(String[] args) {
+		
+		//understand variables & data types
+		
+		int myNumber = 10;
+		double d = 4.5;
+		float f = (float) 4.5; //cast
+		char c = 'g';
+		String s2 = "hello World";
+		
+		int num = 5;
+		String s = "I have " + num + " numbers of mongoes";
+		
+		System.out.println(s);
+	}
+}
---
+++ variables/src/variables/hello2.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class hello2 {
+
+	public static void main(String[] args) {
+		
+		//understand about boolean
+		
+		boolean b = false;
+		b = true;
+		
+		boolean tobe = false;
+		b = tobe || !tobe;
+		if(b) {
+			System.out.println(tobe);
+		}
+
+	}
+
+}
