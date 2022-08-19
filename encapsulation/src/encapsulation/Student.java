f57956b40348f4b98a64d953e7691d960838efc0
modithapahan <modithapahan0@gmail.com>
2022-08-19 08:47:15 +0530

    Encapsulation

---
+++ encapsulation/src/encapsulation/Student.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class Student {
+	private String name;
+	
+	public void setName(String sName) {
+		this.name = sName;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
---
+++ encapsulation/src/encapsulation/main.java
@@ -0,0 +1,13 @@
+package encapsulation;
+
+public class main {
+
+	public static void main(String[] args) {
+		Student st1 = new Student();
+		
+		st1.setName("Moditha");
+		
+		System.out.println("Student name is : " + st1.getName());
+	}
+
+}
