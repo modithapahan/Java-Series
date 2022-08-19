4896f0ef284ea74ec251d18f0fd33c293f2620d3
modithapahan <modithapahan0@gmail.com>
2022-08-19 12:45:57 +0530

    Local Variables

---
+++ localvariables/src/package1/Main.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Phone p1 = new Phone();
+		
+		p1.call();
+
+	}
+
+}
---
+++ localvariables/src/package1/Phone.java
@@ -0,0 +1,16 @@
+package package1;
+
+public class Phone {
+	String name;
+	String color;
+	int battery;
+	double price;
+	
+	public void call() {
+		String callName = "Moditha";
+		System.out.println("Calling to : " + callName);
+	}
+	public void sendText() {}
+	public void findPlaces() {}
+	public void takePhotos() {}
+}
