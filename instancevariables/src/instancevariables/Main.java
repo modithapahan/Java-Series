7e3f97530cea4c9c183621824d10caa687cacc4f
modithapahan <modithapahan0@gmail.com>
2022-08-19 16:23:47 +0530

    Instance Variables

---
+++ instancevariables/src/instancevariables/Main.java
@@ -0,0 +1,15 @@
+package instancevariables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student s1 = new Student();
+		
+		s1.name = "Moditha";
+		s1.age = 22;
+		
+		s1.studentDetails();
+	}
+
+}
---
+++ instancevariables/src/instancevariables/Student.java
@@ -0,0 +1,11 @@
+package instancevariables;
+
+public class Student {
+		String name;
+		int age;
+		
+		public void studentDetails() {
+			System.out.println("Name is : " + this.name);
+			System.out.println("Age is : " + this.age);
+		}
+}
