cd782524f07f85260d0ad1394ed52f11e0258791
modithapahan <modithapahan0@gmail.com>
2022-08-19 08:51:24 +0530

    Classes & Objects

---
+++ class&objects/src/classes/phone.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class phone {
+
+		String name;
+		String color;
+		int battery;
+		double price;
+		
+		public void call() {}
+		public void sendText() {}
+		public void findPlaces() {}
+		public void takePhotos() {}
+
+}
